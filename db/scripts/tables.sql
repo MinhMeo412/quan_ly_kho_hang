8641fcd8eb22e907b520d83b7bbd394c04c4bb11
MinhMeo412 <vq.minh97@gmail.com>
2024-06-04 00:15:27 +0700

    Updated table draft
    Sửa lại 1 số bảng theo nhu cầu phần mềm thực tế

--- db/scripts/tables.sql
+++ db/scripts/tables.sql
@@ -1,11 +1,12 @@
 /*
-- user
-    - id (pk)
+- users
+    - user_id (pk)
     - username
     - password
+    - full_name
     - email
-    - phone number
-    - permission level (fk)
+    - phone_number
+    - level (fk) (các fk nên để tên giống pk của bảng tham chiếu)
 - permission
     - level (pk)
     - name
@@ -13,60 +14,69 @@
 
 
 
+- suppliers
+    - supplier_id (pk)
+    - supplier_name
+    - supplier_address (chỉ cần sử dụng 1 địa chỉ chính đối với NCC)
+    - supplier_email
+    - supplier_phone
+    - description (mô tả về NCC và 1 số main product của NCC)
+
+    *(supplier address không cần tạo bảng vì địa chỉ của supplier không hoạt động
+    giống địa chỉ của customer)
+    *(supplier product không cần thiết, thông tin này có thể đưa vào trong description của supplier)
+
 - category
-    - id (pk)
-    - name
+    - category_id (pk)
+    - category_name
     - description
 - product
-    - id (pk)
-    - name
+    - product_id (pk)
+    - product_name
     - description
-    - category
-- product_variant
-    - id (pk)
-    - product id (fk)
-    - image id (fk)
-    - color
-    - size
     - price
-- image
-    - image id (fk)
+    - category_id (fk)
+- product_image
+    - image_id (fk)
     - image url
+- product_variant
+    - variant_id (pk)
+    - product_id (fk) (composite key)
+    - image_id (fk)
+    - color (composite key)
+    - size (composite key)
 
 
 
-- address
+
+- warehouse address
     - id (pk)
-    - address line 1
-    - address line 2
+    - address line 1 (thông thường 1 warehouse chỉ có 1 địa chỉ)
     - district
     - city
     - postal code
     - country
-
-
-
-- supplier 
-    - id (pk)
-    - name
-- supplier address
-    - supplier id (fk) (composite key)
-    - address id (fk) (composite key)
-    - primary address (boolean)
-- supplier product
-    - product variant id (fk) (composite key)
-    - supplier id (fk) (composite key)
-
-
-
 - warehouse
-    - id (pk)
-    - name
+    - warehouse_id (pk)
+    - warehouse_name
     - address (fk)
 - warehouse stock
-    - warehouse id (fk)
-    - product variant id (fk)
-    - stock quantity
+    - warehouse_id (fk)
+    - variant_id (fk)
+    - quantity
+- inventory check
+    - check_id (pk)
+    - datetime
+    - warehouse_id (fk)
+    - check_by_user_id (fk)
+- checklist details
+    - check_id (fk)
+    - checklist_details_id (pk)
+    - variant_id (fk)
+    - quantity (fk) ref warehouse stock
+    - actual_quantity
+    - quantity discrepancy
+
 
 
 
@@ -76,31 +86,55 @@
     - warehouse id (fk)
     - order date
     - arrival date
+    - create by user (fk)
     - status
-- inbound product
+    - description
+- inbound product details
     - product variant id (fk) (composite key)
     - inbound shipment id (fk) (composite key)
     - amount
 
+- stock transfer
+    - id (pk)
+    - create_date
+    - create by user (fk)
+    - status
+    - from warehouse id (fk)
+    - to warehouse id (fk)
+    - description
+- transfer details
+    - product variant id (fk) (composite key)
+    - outbound shipment id (fk) (composite key)
+    - amount
+Xuất chuyển kho và Xuất giao hàng nên có 2 kiểu phiếu khác nhau
+
 - outbound shipment
     - id (pk)
-    - order date
-    - arrival date
+    - from warehouse id (fk)
+    - create_date
+    - create by user (fk)
+    - to customer (bảng customer hoặc tự nhập)
     - status
-    - address id (fk)
-- outbound product
+    - description
+- outbound product details
     - product variant id (fk) (composite key)
-    - warehouse id (fk) (composite key)
     - outbound shipment id (fk) (composite key)
     - amount
 
-
 !!!
 - procedure sẽ yêu cầu 1 permission level tối thiểu nào đó để thực hiện?
+    (procedure sẽ được gọi dựa trên chức năng được hiển thị qua menu của user, cách của anh là 
+    tạo cho mỗi user 1 menu được hiển thị riêng, level user nào thì tương ứng với các chức năng được
+    hiển thị và sử dụng chức năng đó, admin có đầy đủ menu chức năng, các level user thấp hơn chỉ cần
+    menu không hiển thị các chức năng level đó không được sử dụng là được)
 - 1 product có thể có nhiều variant
+    (confirmed)
 - 1 supplier có thể có nhiều address
+    (đã trả lời ở trên)
 - 1 variant có thể có nhiều supplier
+    (không cần thiết)
 - 1 inbound/outbound shipment có thể có nhiều product variant
+    (confirmed)
 
 TODO:
     - thanh toán???
