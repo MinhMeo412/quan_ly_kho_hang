87b291f4dae285aca1b2bc919ad0abdd97b90eed
cvlp <98164576+cvlp@users.noreply.github.com>
2024-06-07 16:00:29 +0700

    minor update (#25)

--- db/scripts/insert_dummy_data.sql
+++ db/scripts/insert_dummy_data.sql
@@ -1,53 +1,53 @@
-SELECT 'Loading permissions' as 'INFO';
+SELECT 'Loading permissions' AS 'INFO';
 SOURCE dummy_data/load_permission.dump;
 
-SELECT 'Loading users' as 'INFO';
+SELECT 'Loading users' AS 'INFO';
 SOURCE dummy_data/load_user.dump;
 
-SELECT 'Loading tokens' as 'INFO';
+SELECT 'Loading tokens' AS 'INFO';
 SOURCE dummy_data/load_token.dump;
 
-SELECT 'Loading suppliers' as 'INFO';
+SELECT 'Loading suppliers' AS 'INFO';
 SOURCE dummy_data/load_supplier.dump;
 
-SELECT 'Loading categories' as 'INFO';
+SELECT 'Loading categories' AS 'INFO';
 SOURCE dummy_data/load_category.dump;
 
-SELECT 'Loading products' as 'INFO';
+SELECT 'Loading products' AS 'INFO';
 SOURCE dummy_data/load_product.dump;
 
-SELECT 'Loading product variants' as 'INFO';
+SELECT 'Loading product variants' AS 'INFO';
 SOURCE dummy_data/load_product_variant.dump;
 
-SELECT 'Loading warehouse addresses' as 'INFO';
+SELECT 'Loading warehouse addresses' AS 'INFO';
 SOURCE dummy_data/load_warehouse_address.dump;
 
-SELECT 'Loading warehouses' as 'INFO';
+SELECT 'Loading warehouses' AS 'INFO';
 SOURCE dummy_data/load_warehouse.dump;
 
-SELECT 'Loading warehouse stocks' as 'INFO';
+SELECT 'Loading warehouse stocks' AS 'INFO';
 SOURCE dummy_data/load_warehouse_stock.dump;
 
-SELECT 'Loading inventory audits' as 'INFO';
+SELECT 'Loading inventory audits' AS 'INFO';
 SOURCE dummy_data/load_inventory_audit.dump;
 
-SELECT 'Loading inventory audit details' as 'INFO';
+SELECT 'Loading inventory audit details' AS 'INFO';
 SOURCE dummy_data/load_inventory_audit_detail.dump;
 
-SELECT 'Loading inbound shipments' as 'INFO';
+SELECT 'Loading inbound shipments' AS 'INFO';
 SOURCE dummy_data/load_inbound_shipment.dump;
 
-SELECT 'Loading inbound shipment details' as 'INFO';
+SELECT 'Loading inbound shipment details' AS 'INFO';
 SOURCE dummy_data/load_inbound_shipment_detail.dump;
 
-SELECT 'Loading outbound shipments' as 'INFO';
+SELECT 'Loading outbound shipments' AS 'INFO';
 SOURCE dummy_data/load_outbound_shipment.dump;
 
-SELECT 'Loading outbound shipment details' as 'INFO';
+SELECT 'Loading outbound shipment details' AS 'INFO';
 SOURCE dummy_data/load_outbound_shipment_detail.dump;
 
-SELECT 'Loading stock transfers' as 'INFO';
+SELECT 'Loading stock transfers' AS 'INFO';
 SOURCE dummy_data/load_stock_transfer.dump;
 
-SELECT 'Loading stock transfer details' as 'INFO';
+SELECT 'Loading stock transfer details' AS 'INFO';
 SOURCE dummy_data/load_stock_transfer_detail.dump;
--- db/scripts/triggers.sql
+++ db/scripts/triggers.sql
@@ -1,4 +1,24 @@
+
 /*
-Todo:
-    * make triggers to perform deductions whenever an update related to products is made
-*/
+ * Trigger to auto update warehouse stock when inbound shipment status is set to delivered
+ */
+
+/*
+ * Trigger to auto update warehouse stock when inbound shipment status reverted from Delivered to undelivered
+ */
+
+/*
+ * Trigger to auto update warehouse stock before an outbound shipment is created
+ */
+
+ /*
+  * Trigger to auto update warehouse stock before an outbound shipment detail amount is modified
+  */
+
+ /*
+  * Trigger to auto update warehouse stock before a stock transfer order is created
+  */
+
+ /*
+  * Trigger to auto update warehouse stock before a stock transfer detail amount is modified
+  */
