4705a2ec225e6ebb14e3bfbc1b78b68d3112cec9
cvlp <98164576+cvlp@users.noreply.github.com>
2024-06-09 22:19:37 +0700

    improved Procedure executor to support out parameters

--- src/WarehouseManager/Program.cs
+++ src/WarehouseManager/Program.cs
@@ -1,9 +1,16 @@
 ﻿using WarehouseManager.Core;
+using WarehouseManager.Data.Utility;
 
 class Program
 {
     public static void Main(String[] args)
     {
-
+        string connectionString = "server=localhost; user=root; password=7777; database=warehouse";
+        Dictionary<string, object> inParameters = new Dictionary<string, object> {
+            {"input_shit",3}
+        };
+        List<string> outParameters = new List<string> {"output_shit"};
+        Dictionary<string, object> output = Procedure.ExecuteNonQuery(connectionString, "testing_shit", inParameters, outParameters);
+        Console.WriteLine(output["output_shit"]);
     }
 }
