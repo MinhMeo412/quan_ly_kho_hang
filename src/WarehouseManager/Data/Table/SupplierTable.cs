6a43507d5ef55980295d6136ee7c4a2a71625c7c
shynndex <140325390+shynndex@users.noreply.github.com>
2024-06-13 18:06:17 +0700

    Update SupplierTable.cs

--- src/WarehouseManager/Data/Table/SupplierTable.cs
+++ src/WarehouseManager/Data/Table/SupplierTable.cs
@@ -1,11 +1,4 @@
- using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WarehouseManager.Data.Entity;
-using System.Data.SqlClient;
-using System.Data;
 
 namespace WarehouseManager.Data.Table
 {
