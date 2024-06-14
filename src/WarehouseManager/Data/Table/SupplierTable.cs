1b460020326cb461e3f5a489d7dfbeafd513284b
shynndex <140325390+shynndex@users.noreply.github.com>
2024-06-14 18:24:20 +0700

    Update method  (#48)
    * Update Detete Procedure
    
    improved formatting
    
    * Update procedures_delete.sql
    
    improved formatting
    
    * 1st try to finish Supplier and User method
    
    * Update SupplierTable.cs
    
    * Update basic method for WarehouseManagement

--- src/WarehouseManager/Data/Table/SupplierTable.cs
+++ src/WarehouseManager/Data/Table/SupplierTable.cs
@@ -5,5 +5,90 @@ namespace WarehouseManager.Data.Table
     class SupplierTable
     {
         public List<Supplier>? Suppliers { get; private set; }
+        
+        private void Load(string connectionString,string token)
+        {
+            Dictionary<string, object>? inParameters = new Dictionary<string, object>
+            {
+                {"input_token",token }
+            };
+
+            List<List<object>> rawSuppliers = Procedure.ExecuteReader(connectionString, "read_supplier", inParameters);
+
+            List<Supplier> suppliers = new List<Supplier>();
+            foreach(List<object> rawSupplier in rawSuppliers)
+            {
+                Supplier supplier = new Supplier((int)supplierID[0], (string)supplierName[1], (string)supplierDescription[2], (string)supplierAddress[3], (string)supplierEmail[4], (string)supplierPhoneNumber[5], (string)supplierWebsite[6]);
+                suppliers.Add(supplier);
+            }
+            this.Add(permission);
+        }
+
+        public void Add(string connectionString, string token, int supplierID, string supplierName, string? supplierDescription, string? supplierAddress, string? supplierEmail, string? supplierPhoneNumber, string? supplierWebsite)
+        {
+            Dictionary<string, object>? inParameters = new Dictionary<string, object>
+            {
+                {"input_token", token},
+                {"new_supplier_name", supplierName},
+                {"new_supplier_description", supplierDescription},
+                {"new_supplier_address", permissionDescription},
+                {"new_supplier_email", supplierEmail},
+                {"new_supplier_phone_number", supplierPhoneNumber},
+                {"new_supplier_website", supplierWebsite}
+            };
+
+            Procedure.ExecuteNonQuery(connectionString, "create_supplier", inParameters);
+
+            Supplier supplier = new Supplier(supplierName, supplierDescription, permissionDescription, supplierEmail, supplierPhoneNumber, supplierWebsite);
+
+
+            this.Suppliers ??= new List<Supplier>();
+            this.Suppliers.Add(supplier);
+        }
+        
+        public void Update(string connectionString, string token, int supplierID, string supplierName, string? supplierDescription, string? supplierAddress, string? supplierEmail, string? supplierPhoneNumber, string? supplierWebsite)
+        {
+            Dictionary<string, object>? inParameters = new Dictionary<string, object>
+            {
+                {"input_token", token},
+                {"new_supplier_name", supplierName},
+                {"new_supplier_description", supplierDescription},
+                {"new_supplier_address", supplierAddress},
+                {"new_supplier_email", supplierEmail},
+                {"new_supplier_phone_number", supplierPhoneNumber},
+                {"new_supplier_website", supplierWebsite}
+            };
+
+            Procedure.ExecuteNonQuery(connectionString, "update_supplier", inParameters);
+
+            var supplier=this.Suppliers?.FirstOrDefault(s => s.SupplierID == supplierID);
+
+            if (supplier != null)
+            {
+                supplier.SupplierName = supplierName;
+                supplier.SupplierDescription = supplierDescription;
+                supplier.SupplierAddress = supplierName;
+                supplier.SupplierEmail = supplierEmail;
+                supplier.SupplierPhoneNumber = supplierPhoneNumber;
+                supplier.SupplierWebsite = supplierWebsite;
+            }
+        }
+
+        public void Delete(string connectionString, string token, int supplierID)
+        {
+            Dictionary<string, object>? inParameters = new Dictionary<string, object>
+            {
+                {"input_token", token},
+                {"target_supplier_id", supplierID},
+            };
+            Procedure.ExecuteNonQuery(connectionString, "delete_supplier", inParameters);
+
+            var supplier = this.Suppliers?.FirstOrDefault(s => s.SupplierID == supplierID);
+            if (supplier != null)
+            {
+                this.Suppliers ??= new List<Supplier>();
+                this.Suppliers.Remove(supplier);
+            }
+        }
     }
 }
--- src/WarehouseManager/Data/Table/UserTable.cs
+++ src/WarehouseManager/Data/Table/UserTable.cs
@@ -5,5 +5,71 @@ namespace WarehouseManager.Data.Table
     class UserTable
     {
         public List<User>? Users { get; private set; }
+
+        private void Load(string connectionString,string token)
+        {
+            Dictionary<string, object>? inParameters = new Dictionary<string, object>{
+                {"input_token", token}
+            };
+
+            List<List<object>> rawUsers = Procedure.ExecuteReader(connectionString, "read_user", inParameters);
+
+            List<User> users = new List<User>();
+            foreach (List<object> rawUser in rawUser)
+            {
+                User user = new User((int)userID[0], (string)userName[1], (string)userPassword[2], (string)userFullName[3], (string)userEmail[4],(string)userPhoneNumber[5], (string)permissionLevel[6]);
+                users.Add(user);
+            }
+
+            this.Users = users;
+        }
+
+        private void Add(string connectionString, string token, int userID, string userName, string userPassword, string? userFullName, string? userEmail, string? userPhoneNumber, int permissionLevel)
+        {
+            Dictionary<string, object>? inParameters = new Dictionary<string, object>{
+                {"input_token", token},
+                {"new_user_name", userName},
+                {"new_user_password", userPassword},
+                {"new_user_full_name", userFullName},
+                {"new_user_email", userEmail},
+                {"new_user_phone_number", userPhoneNumber},
+                {"new_permission_level", permissionLevel}
+            };
+            Procedure.ExecuteNonQuery(connectionString, "create_user", inParameters);
+
+            User user = new User(userName, userPassword, userFullName, userEmail, userPhoneNumber, permissionLevel);
+
+            this.Users ??= new List<User>();
+            this.Users.Add(user);
+        }
+
+        public void Update(string connectionString, string token, int userID, string userName, string userPassword, string? userFullName, string? userEmail, string? userPhoneNumber, int permissionLevel)
+            {
+            Dictionary<string, object>? inParameters = new Dictionary<string, object>
+            {
+               {"input_token", token},
+                {"new_user_name", userName},
+                {"new_user_password", userPassword},
+                {"new_user_full_name", userFullName},
+                {"new_user_email", userEmail},
+                {"new_user_phone_number", userPhoneNumber},
+                {"new_permission_level", permissionLevel}
+            };
+
+            Procedure.ExecuteNonQuery(connectionString, "update_user", inParameters);
+
+            var user = this.Users?.FirstOrDefault(u => u.UserID == userID);
+            if (user != null)
+            {
+                user.UserName = userName;
+                user.UserPassword = userPassword;
+                user.UserFullName = userName;
+                user.PermissionDescription = permissionDescription;
+                user.UserEmail = userEmail;
+                user.UserPhoneNumber = userPhoneNumber;
+                user.PermissionLevel = permissionLevel;
+            }
+        }
     }
 }
+
