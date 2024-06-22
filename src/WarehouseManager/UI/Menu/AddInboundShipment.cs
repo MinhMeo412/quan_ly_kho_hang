3bfa6155cb1ac8f80201d84f119ea53483f771b5
VQMinh <vq.minh97@gmail.com>
2024-06-22 17:03:01 +0700

    Add Display for AddIn-Out-Transfer-Warehouse

--- src/WarehouseManager/UI/Menu/AddInboundShipment.cs
+++ src/WarehouseManager/UI/Menu/AddInboundShipment.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using Terminal.Gui;
 using WarehouseManager.UI.Utility;
 
@@ -5,10 +6,6 @@ namespace WarehouseManager.UI.Menu
 {
     public static class AddInboundShipment
     {
-       /*
-            Todo.
-            Thêm phiếu nhập .
-        */
         public static void Display()
         {
             Application.Top.RemoveAll();
@@ -19,8 +16,166 @@ namespace WarehouseManager.UI.Menu
 
             var userPermissionLabel = UIComponent.UserPermissionLabel("Username", "Permission");
 
-            mainWindow.Add(errorLabel, userPermissionLabel);
-        }
+            var separatorLine = UIComponent.SeparatorLine();
 
+            var container = new FrameView()
+            {
+                X = Pos.Center(),
+                Y = 1,
+                Width = Dim.Percent(85),
+                Height = Dim.Percent(30)
+            };
+
+            var warehouseLabel = new Label("Warehouse:")
+            {
+                X = 3,
+                Y = 1
+            };
+
+            var warehouseInput = new TextField("")
+            {
+                X = 20,
+                Y = Pos.Top(warehouseLabel),
+                Width = 50,
+            };
+
+            var supplierNameLabel = new Label("Supplier Name:")
+            {
+                X = 3,
+                Y = Pos.Bottom(warehouseLabel) + 2
+            };
+
+            var supplierNameInput = new TextField("")
+            {
+                X = 20,
+                Y = Pos.Top(supplierNameLabel),
+                Width = 50,
+            };
+
+            var descriptionLabel = new Label("Description:")
+            {
+                X = 3,
+                Y = Pos.Bottom(warehouseLabel) + 6
+            };
+
+            var descriptionInput = new TextView()
+            {
+                X = 20,
+                Y = Pos.Bottom(supplierNameInput) + 2,
+                Width = 50,
+                Height = 3,
+                Text = "",
+            };
+
+            var dateLabel = new Label("Date:")
+            {
+                X = 110,
+                Y = 1
+            };
+
+            var dateInput = new TextField("")
+            {
+                X = 120,
+                Y = Pos.Top(dateLabel),
+                Width = 50,
+            };
+
+            var userLabel = new Label("User:")
+            {
+                X = 110,
+                Y = Pos.Bottom(dateLabel) + 2
+            };
+
+            var userInput = new TextField("")
+            {
+                X = 120,
+                Y = Pos.Top(userLabel),
+                Width = 50,
+            };
+
+            var statusLabel = new Label("Status:")
+            {
+                X = 110,
+                Y = Pos.Bottom(userLabel) + 2
+            };
+
+            var options = new string[] { "Processing", "Completed" };
+
+            var statusBox = new ComboBox(options)
+            {
+                X = 120,
+                Y = Pos.Top(statusLabel),
+                Width = 50,
+                Height = 4
+            };
+
+
+            var tableContainer = new FrameView()
+            {
+                X = Pos.Center(),
+                Y = Pos.Bottom(container) + 2,
+                Width = Dim.Percent(95),
+                Height = Dim.Fill(7),
+            };
+
+            var dataTable = new DataTable();
+            dataTable.Columns.Add("ID", typeof(int));
+            dataTable.Columns.Add("Name", typeof(string));
+            dataTable.Columns.Add("Age", typeof(int));
+            dataTable.Rows.Add(1, "Alice", 30);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(3, "Charlie", 35);
+            dataTable.Rows.Add(1, "Alice", 30);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+            dataTable.Rows.Add(2, "Bob", 25);
+
+            var tableView = UIComponent.Table(dataTable);
+
+            //Tạo nút save
+            var saveButton = new Button("Save", is_default: true)
+            {
+                X = Pos.Center(),
+                Y = Pos.Bottom(tableContainer) + 2
+            };
+
+            //Khi nhấn nút save
+            saveButton.Clicked += () =>
+            {
+                MessageBox.Query("Save", "", "OK");
+            };
+
+
+            tableContainer.Add(tableView);
+            container.Add(warehouseLabel, warehouseInput, descriptionLabel, dateLabel, dateInput, userLabel, userInput, statusLabel, statusBox, supplierNameLabel, supplierNameInput, descriptionInput);
+            mainWindow.Add(container, tableContainer, separatorLine, errorLabel, userPermissionLabel, saveButton);
+        }
     }
 }
--- src/WarehouseManager/UI/Menu/AddWarehouse.cs
+++ src/WarehouseManager/UI/Menu/AddWarehouse.cs
@@ -5,10 +5,6 @@ namespace WarehouseManager.UI.Menu
 {
     public static class AddWarehouse
     {
-      /*
-            Todo.
-            Thêm nhà kho.
-        */
         public static void Display()
         {
             Application.Top.RemoveAll();
@@ -19,8 +15,125 @@ namespace WarehouseManager.UI.Menu
 
             var userPermissionLabel = UIComponent.UserPermissionLabel("Username", "Permission");
 
-            mainWindow.Add(errorLabel, userPermissionLabel);
-        }
+            var separatorLine = UIComponent.SeparatorLine();
 
+            var container = new FrameView()
+            {
+                X = Pos.Center(),
+                Y = Pos.Center(),
+                Width = Dim.Percent(65),
+                Height = Dim.Percent(50)
+            };
+
+            var warehouseNameLabel = new Label("Warehouse Name:")
+            {
+                X = 3,
+                Y = 1
+            };
+            var warehouseNameInput = new TextField("")
+            {
+                X = 25,
+                Y = Pos.Top(warehouseNameLabel),
+                Width = 50,
+            };
+
+            var warehouseAddressLabel = new Label("Warehouse Address:")
+            {
+                X = 3,
+                Y = Pos.Bottom(warehouseNameLabel) + 1
+            };
+            var warehouseAddressInput = new TextField("")
+            {
+                X = 25,
+                Y = Pos.Top(warehouseAddressLabel),
+                Width = 50,
+            };
+
+            var districtLabel = new Label("District:")
+            {
+                X = 3,
+                Y = Pos.Bottom(warehouseNameLabel) + 3
+            };
+            var districtInput = new TextField("")
+            {
+                X = 25,
+                Y = Pos.Top(districtLabel),
+                Width = 50,
+            };
+
+            var cityLabel = new Label("City:")
+            {
+                X = 3,
+                Y = Pos.Bottom(warehouseNameLabel) + 5
+            };
+            var cityInput = new TextField("")
+            {
+                X = 25,
+                Y = Pos.Top(cityLabel),
+                Width = 50,
+            };
+
+            var countryLabel = new Label("Country:")
+            {
+                X = 3,
+                Y = Pos.Bottom(warehouseNameLabel) + 7
+            };
+            var countryInput = new TextField("")
+            {
+                X = 25,
+                Y = Pos.Top(countryLabel),
+                Width = 50,
+            };
+
+            var postalCodeLabel = new Label("Postal Code:")
+            {
+                X = 3,
+                Y = Pos.Bottom(warehouseNameLabel) + 9
+            };
+            var postalCodeInput = new TextField("")
+            {
+                X = 25,
+                Y = Pos.Top(postalCodeLabel),
+                Width = 50,
+            };
+
+            //Tạo nút save
+            var saveButton = new Button("Save", is_default: true)
+            {
+                X = Pos.Center(),
+                Y = Pos.Bottom(warehouseNameLabel) + 15
+            };
+
+            //Khi nhấn nút save
+            saveButton.Clicked += () =>
+            {
+                var message = $"Name: {warehouseNameInput.Text}\nAddress: {warehouseAddressInput.Text}\nDistrict: {districtInput.Text}\nCity: {cityInput.Text}\nCountry: {countryInput.Text}\nPostal Code: {postalCodeInput.Text}";
+                var dialog = new Dialog("Save", 60, 20);
+
+                var messageLabel = new Label(message)
+                {
+                    X = 1,
+                    Y = 1,
+                    Width = Dim.Fill() - 2,
+                    Height = Dim.Fill() - 2,
+                    TextAlignment = TextAlignment.Left,
+                };
+
+                var okButton = new Button("OK", is_default: true)
+                {
+                    X = Pos.Center(),
+                    Y = Pos.Bottom(messageLabel) + 3
+                };
+                okButton.Clicked += () => Application.RequestStop();
+
+                dialog.Add(messageLabel, okButton);
+                Application.Run(dialog);
+            };
+
+            container.Add(warehouseNameLabel, warehouseNameInput, warehouseAddressLabel, warehouseAddressInput, districtLabel, districtInput, cityLabel, cityInput, countryLabel, countryInput, postalCodeLabel, postalCodeInput, saveButton);
+            mainWindow.Add(container, errorLabel, userPermissionLabel, separatorLine);
+
+
+        }
     }
 }
