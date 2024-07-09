2c2daa6c9a00d861e50f335695c23a21030bcb9d
shynndex <140325390+shynndex@users.noreply.github.com>
2024-07-10 00:04:55 +0700

    Add Supplier Logic

---
+++ src/WarehouseManager/Core/Pages/Suppliers/AddSupplierLogic.cs
@@ -0,0 +1,30 @@
+using WarehouseManager.Data.Entity;
+
+using System.Data;
+
+
+namespace WarehouseManager.Core.Pages
+{
+    public static class AddSupplierLogic
+    {
+        public static void AddSupplier(int supplierID, string supplierName, string? supplierDescription, string? supplierAddress, string? supplierEmail, string? supplierPhoneNumber, string? supplierWebsite)
+        {
+            Program.Warehouse.SupplierTable.Add(supplierID, supplierName, supplierDescription, supplierAddress, supplierEmail, supplierPhoneNumber, supplierWebsite);
+        }
+
+
+        public static void Save(string supplierName, string? supplierDescription, string? supplierAddress, string? supplierEmail, string? supplierPhoneNumber, string? supplierWebsite)
+        {
+            int supplierID = GetCurrentHighestSupplierID() + 1;
+            AddSupplier(supplierID, supplierName, supplierDescription, supplierAddress, supplierEmail, supplierPhoneNumber, supplierWebsite);
+
+        }
+        private static int GetCurrentHighestSupplierID()
+        {
+            List<Supplier> suppliers = Program.Warehouse.SupplierTable.Suppliers ?? new List<Supplier>();
+            int hightestSupplierID = suppliers.Max(s => s.SupplierID);
+            return hightestSupplierID;
+        }
+
+    }
+}
