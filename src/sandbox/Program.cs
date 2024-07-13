c7553e6c3ff8e5704e922f50e5898b3b7b229927
cvlp <98164576+cvlp@users.noreply.github.com>
2024-07-13 10:58:44 +0700

    completed home menu

--- src/sandbox/Program.cs
+++ src/sandbox/Program.cs
@@ -1,59 +1,18 @@
-﻿using Figgle;
+﻿
+using Figgle;
 
 class Program
 {
     static void Main()
     {
 
-        string time = GetTime();
-        Console.WriteLine(time);
 
-        for (int i = 0; i < GetStringWidth(time); i++)
-        {
-            Console.Write("-");
-        }
-        Console.WriteLine();
-
-        Console.WriteLine(GetStringWidth(time));
+        
     }
 
-    public static string GetTime()
-    {
-        DateTime now = DateTime.Now;
+    
+    
 
-        // Format the time as HH:mm
-        string currentTime = now.ToString("HH:mm");
-        string timeWithSpaces = "";
-        foreach (char letter in currentTime)
-        {
-            timeWithSpaces += $"{letter} ";
-        }
-        timeWithSpaces = timeWithSpaces.Trim();
 
-        string asciiArt = FiggleFonts.Mini.Render(timeWithSpaces).TrimEnd();
 
-        return asciiArt;
-    }
-
-    public static int GetStringWidth(string input)
-    {
-        List<string> phrases = input.Split('\n').ToList();
-        int maxWidth = 0;
-        foreach (string phrase in phrases)
-        {
-            if (phrase.Length > maxWidth)
-            {
-                maxWidth = phrase.Length;
-            }
-        }
-        return maxWidth;
-    }
-
-    public static int GetStringHeight(string input)
-    {
-        List<string> phrases = input.Split('\n').ToList();
-        int height = phrases.Count;
-
-        return height;
-    }
 }
