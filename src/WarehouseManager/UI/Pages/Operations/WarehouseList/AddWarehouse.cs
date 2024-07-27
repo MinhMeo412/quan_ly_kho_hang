8353ecd3a1381b477d8add075db99217a32d7fae
cvlp <98164576+cvlp@users.noreply.github.com>
2024-07-27 19:21:21 +0700

    Fixed add warehouse feature

--- src/WarehouseManager/UI/Pages/Operations/WarehouseList/AddWarehouse.cs
+++ src/WarehouseManager/UI/Pages/Operations/WarehouseList/AddWarehouse.cs
@@ -12,7 +12,7 @@ namespace WarehouseManager.UI.Pages
             var mainWindow = UIComponent.LoggedInMainWindow("Add New Warehouse");
             Application.Top.Add(mainWindow);
 
-            var errorLabel = UIComponent.AnnounceLabel("Error Message Here");
+            var errorLabel = UIComponent.AnnounceLabel();
 
             var userPermissionLabel = UIComponent.UserPermissionLabel();
 
@@ -22,8 +22,8 @@ namespace WarehouseManager.UI.Pages
             {
                 X = Pos.Center(),
                 Y = Pos.Center(),
-                Width = Dim.Percent(65),
-                Height = 20
+                Width = Dim.Percent(75),
+                Height = 17
             };
 
             var warehouseNameLabel = new Label("Warehouse Name:")
@@ -31,127 +31,116 @@ namespace WarehouseManager.UI.Pages
                 X = 3,
                 Y = 1
             };
-            var warehouseNameInput = new TextField("")
-            {
-                X = 25,
-                Y = Pos.Top(warehouseNameLabel),
-                Width = Dim.Percent(65),
-            };
+
 
             var warehouseAddressLabel = new Label("Warehouse Address:")
             {
                 X = 3,
                 Y = Pos.Bottom(warehouseNameLabel) + 1
             };
+
+            var warehouseNameInput = new TextField("")
+            {
+                X = Pos.Right(warehouseAddressLabel) + 1,
+                Y = Pos.Top(warehouseNameLabel),
+                Width = Dim.Fill(3)
+            };
+
             var warehouseAddressInput = new TextField("")
             {
-                X = 25,
+                X = Pos.Right(warehouseAddressLabel) + 1,
                 Y = Pos.Top(warehouseAddressLabel),
-                Width = Dim.Percent(65),
+                Width = Dim.Fill(3),
             };
+
 
             var districtLabel = new Label("District:")
             {
                 X = 3,
-                Y = Pos.Bottom(warehouseNameLabel) + 3
+                Y = Pos.Bottom(warehouseAddressLabel) + 1
             };
             var districtInput = new TextField("")
             {
-                X = 25,
+                X = Pos.Right(warehouseAddressLabel) + 1,
                 Y = Pos.Top(districtLabel),
-                Width = Dim.Percent(65),
-            };
-
-            var cityLabel = new Label("City:")
-            {
-                X = 3,
-                Y = Pos.Bottom(warehouseNameLabel) + 5
-            };
-            var cityInput = new TextField("")
-            {
-                X = 25,
-                Y = Pos.Top(cityLabel),
-                Width = Dim.Percent(65),
-            };
-
-            var countryLabel = new Label("Country:")
-            {
-                X = 3,
-                Y = Pos.Bottom(warehouseNameLabel) + 7
-            };
-            var countryInput = new TextField("")
-            {
-                X = 25,
-                Y = Pos.Top(countryLabel),
-                Width = Dim.Percent(65),
+                Width = Dim.Fill(3)
             };
 
             var postalCodeLabel = new Label("Postal Code:")
             {
                 X = 3,
-                Y = Pos.Bottom(warehouseNameLabel) + 9
+                Y = Pos.Bottom(districtLabel) + 1
             };
             var postalCodeInput = new TextField("")
             {
-                X = 25,
+                X = Pos.Right(warehouseAddressLabel) + 1,
                 Y = Pos.Top(postalCodeLabel),
-                Width = Dim.Percent(65),
+                Width = Dim.Fill(3)
+            };
+
+            var cityLabel = new Label("City:")
+            {
+                X = 3,
+                Y = Pos.Bottom(postalCodeLabel) + 1
+            };
+            var cityInput = new TextField("")
+            {
+                X = Pos.Right(warehouseAddressLabel) + 1,
+                Y = Pos.Top(cityLabel),
+                Width = Dim.Fill(3)
+            };
+
+            var countryLabel = new Label("Country:")
+            {
+                X = 3,
+                Y = Pos.Bottom(cityLabel) + 1
+            };
+            var countryInput = new TextField("")
+            {
+                X = Pos.Right(warehouseAddressLabel) + 1,
+                Y = Pos.Top(countryLabel),
+                Width = Dim.Fill(3)
             };
 
             //Tạo nút save
             var saveButton = new Button("Save", is_default: true)
             {
                 X = Pos.Center(),
-                Y = Pos.AnchorEnd(3)
+                Y = Pos.Bottom(countryLabel) + 1
             };
 
             //Khi nhấn nút save
             saveButton.Clicked += () =>
             {
-                var message = $"Name: {warehouseNameInput.Text}\nAddress: {warehouseAddressInput.Text}\nDistrict: {districtInput.Text}\nCity: {cityInput.Text}\nCountry: {countryInput.Text}\nPostal Code: {postalCodeInput.Text}";
-                var dialog = new Dialog("Save", 60, 20);
+                try
+                {
+                    AddWarehouseLogic.Save(
+                        $"{warehouseNameInput.Text}",
+                        $"{warehouseAddressInput.Text}",
+                        $"{districtInput.Text}",
+                        $"{postalCodeInput.Text}",
+                        $"{cityInput.Text}",
+                        $"{countryInput.Text}"
+                    );
 
-                var messageLabel = new Label(message)
-                {
-                    X = 1,
-                    Y = 1,
-                    Width = Dim.Fill() - 2,
-                    Height = Dim.Fill() - 2,
-                    TextAlignment = TextAlignment.Left,
-                };
+                    errorLabel.Text = $"Warehouse created successfully!";
+                    errorLabel.ColorScheme = UIComponent.AnnounceLabelSuccessColor();
 
-                var okButton = new Button("OK", is_default: true)
+                    warehouseNameInput.Text = "";
+                    warehouseAddressInput.Text = "";
+                    districtInput.Text = "";
+                    postalCodeInput.Text = "";
+                    cityInput.Text = "";
+                    countryInput.Text = "";
+                }
+                catch (Exception ex)
                 {
-                    X = Pos.Center(),
-                    Y = Pos.Bottom(messageLabel) + 3
-                };
-                okButton.Clicked += () =>
-                {
-                    try
-                    {
-                        bool added = AddWarehouseLogic.AddWarehouse($"{warehouseNameInput.Text}");
-                        if (added)
-                        {
-                            errorLabel.Text = "";
-                            MessageBox.Query("Success", "Warehouse added successfully", "OK");
-                        }
-                        else
-                        {
-                            MessageBox.ErrorQuery("Error", "Cannot add more than 10 warehouses", "OK");
-                        }
-                        Application.RequestStop();
-                    }
-                    catch (Exception ex)
-                    {
-                        errorLabel.Text = $"Error: {ex.Message}";
-                        Application.RequestStop();
-                    }
-                };
-                dialog.Add(messageLabel, okButton);
-                Application.Run(dialog);
+                    errorLabel.Text = $"Error: {ex.Message}";
+                    errorLabel.ColorScheme = UIComponent.AnnounceLabelErrorColor();
+                }
             };
 
-            container.Add(warehouseNameLabel, warehouseNameInput, warehouseAddressLabel, warehouseAddressInput, districtLabel, districtInput, cityLabel, cityInput, countryLabel, countryInput, postalCodeLabel, postalCodeInput, saveButton);
+            container.Add(warehouseNameLabel, warehouseNameInput, warehouseAddressLabel, warehouseAddressInput, districtLabel, districtInput, postalCodeLabel, postalCodeInput, cityLabel, cityInput, countryLabel, countryInput, saveButton);
             mainWindow.Add(container, errorLabel, userPermissionLabel, separatorLine);
         }
     }
